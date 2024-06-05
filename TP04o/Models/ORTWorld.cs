d32c0a93016df150b6d09548ee53e4a922d8626a
iachuDyner <48313765@est-ort.edu.ar>
2024-06-05 08:49:25 -0300

    Avanzamos el Trabajo

---
+++ TP04o/Models/ORTWorld.cs
@@ -0,0 +1,62 @@
+public class ORTWorld{
+public static List<string> ListaDestinos {get; private set;}
+public static List<string> ListaHoteles {get; private set;}
+public static List<string> ListaAereos {get; private set;}
+public static List<string> ListaExcursiones {get; private set;}
+public static Dictionary<string, Paquete> Paquetes {get; private set;}
+
+public ORTWorld()
+{
+    ListaDestinos[0] = "Argentina";
+    ListaDestinos[1] = "España" ;
+    ListaDestinos[2] = "Mexico";
+    ListaDestinos[3] = "Camerun";
+    ListaDestinos[4] = "Paises Bajos";
+    ListaDestinos[5] = "Portugal";
+    ListaDestinos[6] = "Australia";
+    ListaDestinos[7] = "Colombia";
+    ListaDestinos[8] = "Grecia";
+    ListaDestinos[9] = "Japon";
+
+    ListaHoteles[0] = "Hotel1.jpg";
+    ListaHoteles[1] = "Hotel2.jpg";
+    ListaHoteles[2] = "Hotel3.jpg";
+    ListaHoteles[3] = "Hotel4.jpg";
+    ListaHoteles[4] = "Hotel5.jpg";
+    ListaHoteles[5] = "Hotel6.jpg";
+    ListaHoteles[6] = "Hotel7.jpg";
+    ListaHoteles[7] = "Hotel8.jpg";
+    ListaHoteles[8] = "Hotel9.jpg";
+    ListaHoteles[9] = "Hotel10.jpg";
+
+    ListaAereos[0] = "Aereolineas1.png";
+    ListaAereos[1] = "Aereolineas2.png";
+    ListaAereos[2] = "Aereolineas3.png";
+    ListaAereos[3] = "Aereolineas4.png";
+    ListaAereos[4] = "Aereolineas5.png";
+    ListaAereos[5] = "Aereolineas6.png";
+    ListaAereos[6] = "Aereolineas7.png";
+    ListaAereos[7] = "Aereolineas8.png";
+    ListaAereos[8] = "Aereolineas9.png";
+    ListaAereos[9] = "Aereolineas10.png";
+
+    ListaExcursiones[0] = "excursion1.jpg";
+    ListaExcursiones[1] = "excursion2.jpg";
+    ListaExcursiones[2] = "excursion3.jpg";
+    ListaExcursiones[3] = "excursion4.jpg";
+    ListaExcursiones[4] = "excursion5.jpg";
+    ListaExcursiones[5] = "excursion6.jpg";
+    ListaExcursiones[6] = "excursion7.jpg";
+    ListaExcursiones[7] = "excursion8.jpg";
+    ListaExcursiones[8] = "excursion9.jpg";
+    ListaExcursiones[9] = "excursion10.jpg";
+
+
+
+
+}
+
+
+    
+
+}
